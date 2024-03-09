e4de03d49060dc1b09857a02d918b7b3a1d7adce
Ahmedelkazaz39 <Ahmedelkazaz39>
2024-03-09 10:39:39 +0200

    add border raduis for modal sheet

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/views/widgets/add_note.dart';
 import 'package:todo_app/views/widgets/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
@@ -9,7 +10,11 @@ class NotesView extends StatelessWidget {
     return   Scaffold(
       floatingActionButton: FloatingActionButton(
         onPressed:  () {
-          showModalBottomSheet(context: context, builder: (context){
+          showModalBottomSheet(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16),
+            ),
+            context: context, builder: (context){
             return const AddNoteBottomSheet() ;
           });
         },
@@ -22,11 +27,4 @@ class NotesView extends StatelessWidget {
 
 
 
-class AddNoteBottomSheet extends StatelessWidget {
-  const AddNoteBottomSheet({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return   Container();
-  }
-}
