983cb677d00ade380e87bd170b45517aac3f4845
Ahmedelkazaz39 <Ahmedelkazaz39>
2024-03-10 23:23:04 +0200

    create add button

--- lib/views/widgets/add_note.dart
+++ lib/views/widgets/add_note.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/constants.dart';
 import 'package:todo_app/views/widgets/custom_text_field.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
@@ -6,25 +7,58 @@ class AddNoteBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const  Padding(
-      padding:   EdgeInsets.symmetric(horizontal: 16),
-      child:   Column(
-        children: [
-          SizedBox(
-            height: 32,
-          ),
-          CustomTextField(
-            hint: 'Title',
-            maxline: 1,
-          ),
-          SizedBox(
-            height: 24,
-          ),
-           CustomTextField(
-            hint: 'Content',
-            maxline: 5,
-          ),
-        ],
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 32,
+            ),
+            CustomTextField(
+              hint: 'Title',
+              maxline: 1,
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            CustomTextField(
+              hint: 'Content',
+              maxline: 5,
+            ),
+             SizedBox(
+              height: 24,
+            ),
+            CustomButton(),
+             SizedBox(
+              height: 8,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 50,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(6),
+        color: kPrimaryColor,
+      ),
+      child: const Center(
+        child: Text('Add',
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),),
       ),
     );
   }
