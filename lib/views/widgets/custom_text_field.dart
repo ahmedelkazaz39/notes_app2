c4d0bcb41f9b5d2f19109e0a72a4747b702dc5a1
Ahmedelkazaz39 <Ahmedelkazaz39>
2024-03-11 14:37:37 +0200

    edit view ui done

--- lib/views/widgets/custom_text_field.dart
+++ lib/views/widgets/custom_text_field.dart
@@ -2,18 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:todo_app/constants.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key, required this.hint,   this.maxline=1});
-  final String hint ;
+  const CustomTextField({super.key, required this.hint, this.maxline = 1});
+  final String hint;
   final int maxline;
 
   @override
   Widget build(BuildContext context) {
     return TextField(
+      textAlignVertical: TextAlignVertical.center,
       cursorColor: kPrimaryColor,
       maxLines: maxline,
       decoration: InputDecoration(
+        // contentPadding:
+        //     EdgeInsets.symmetric(vertical: 5.0), // Adjust vertical padding
+
         hintText: hint,
-         hintStyle:const TextStyle(
+        hintStyle: const TextStyle(
           color: kPrimaryColor,
         ),
         border: buildBorder(),
--- lib/views/widgets/edit_view_body.dart
+++ lib/views/widgets/edit_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app/views/widgets/custom_appbar.dart';
+import 'package:todo_app/views/widgets/custom_text_field.dart';
 
 class EditViewBody extends StatelessWidget {
   const EditViewBody({super.key});
@@ -7,13 +8,24 @@ class EditViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Padding(
-     padding: EdgeInsets.only(top: 24, left: 24, right: 24),
-      child:   SafeArea(
+      padding: EdgeInsets.only(top: 24, left: 24, right: 24),
+      child: SafeArea(
         child: Column(
           children: [
             CustomAppBar(
               title: 'Edit',
               icon: Icons.done,
+            ),
+             SizedBox(
+              height: 32,
+            ),
+            CustomTextField(hint: 'Title'),
+             SizedBox(
+              height: 16,
+            ),
+            CustomTextField(
+              hint: 'Content',
+              maxline: 5,
             ),
           ],
         ),
